fabe4454ff400490332d70d12f1981293ac06297
Confidence Yobo <confiyobo@gmail.com>
2022-03-16 17:51:09 +0100

    Make cron job run every 23 hours

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,7 +6,7 @@ import { NonceManager } from "@ethersproject/experimental";
 
 const privateKey = process.env.PRIVATE_KEY;
 
-export const releaseInitialWhitelistPayment = functions.pubsub.schedule('*/9 * * * *').timeZone('America/New_York')
+export const releaseInitialWhitelistPayment = functions.pubsub.schedule('* */23 * * *').timeZone('America/New_York')
   .onRun(async (context) => {
     const contract = getCacttTokenContract();
     
