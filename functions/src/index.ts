a61468c68d90143acfbb61b9debed78e9cd98f15
Confidence Yobo <confiyobo@gmail.com>
2022-04-04 10:49:37 +0100

    Increase schedule time

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,7 +6,7 @@ import { NonceManager } from "@ethersproject/experimental";
 
 const privateKey = process.env.PRIVATE_KEY;
 
-export const releaseInitialWhitelistPayment = functions.pubsub.schedule('* */1 * * *').timeZone('America/New_York')
+export const releaseInitialWhitelistPayment = functions.pubsub.schedule('0 */1 * * *').timeZone('America/New_York')
   .onRun(async (context) => {
     const contract = getCacttTokenContract();
     
@@ -19,7 +19,7 @@ export const releaseInitialWhitelistPayment = functions.pubsub.schedule('* */1 *
     await tx.wait();
   });
 
-export const releaseMonthlyWhitelistPayment = functions.pubsub.schedule('* */5 * * *').timeZone('America/New_York')
+export const releaseMonthlyWhitelistPayment = functions.pubsub.schedule('0 */5 * * *').timeZone('America/New_York')
   .onRun(async (context) => {
     const contract = getCacttTokenContract();
 
